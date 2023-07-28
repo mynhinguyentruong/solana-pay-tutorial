b8cf9820fe2c4d43f7a85ae9ebd7ee730244a584
Nhi Nguyen <64499617+mynhinguyentruong@users.noreply.github.com>
2023-07-28 12:49:00 -0400

    modified landing page

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { clusterApiUrl, Connection, Keypair, LAMPORTS_PER_SOL, PublicKey } from 
 import Image from 'next/image'
 import { writeFile } from 'fs/promises'
 
-export async function createMerchantWalletKeypair(): Promise<Keypair> {
+async function createMerchantWalletKeypair(): Promise<Keypair> {
   let keypair: Keypair | undefined
   if (!process.env.MERCHANT_WALLET) {
     console.log("Generating new keypair as merchant wallet")
@@ -23,7 +23,7 @@ export async function createMerchantWalletKeypair(): Promise<Keypair> {
   return keypair
 }
 
-export async function getDevnetSolToken(pubKey: PublicKey) {
+async function getDevnetSolToken(pubKey: PublicKey) {
   const connection = new Connection(clusterApiUrl('devnet'))
   const signature = await connection.requestAirdrop(pubKey, 1 * LAMPORTS_PER_SOL)
   const { blockhash, lastValidBlockHeight } = await connection.getLatestBlockhash();
@@ -45,14 +45,13 @@ export default async function Home() {
    
     <section>
     <div className="relative flex justify-center max-h-full overflow-hidden lg:px-0 md:px-12">
-      <div className="relative z-10 flex flex-col flex-1 px-4 py-10 bg-white shadow-2xl lg:py-24 md:flex-none md:px-28 sm:justify-center">
+      <div className="min-h-screen relative z-10 flex flex-col flex-1 px-4 py-10 bg-white shadow-2xl lg:py-24 md:flex-none md:px-28 sm:justify-center">
         <div className="w-full max-w-md mx-auto md:max-w-sm md:px-0 md:w-96 sm:px-4">
           <div className="flex flex-col">
             <div>
               <h2 className="text-4xl text-black">Let&rsquo;s get started!</h2>
               <p className="mt-2 text-sm text-gray-500">
-                Complete the details below so I can process your request and then
-                schedule a time to discuss your goals.
+                Provide a name for your Merchant Store
               </p>
             </div>
           </div>
@@ -62,35 +61,15 @@ export default async function Home() {
             <div className="mt-4 space-y-6">
               <div>
                 <label className="block mb-3 text-sm font-medium text-gray-600" id="name">
-                  Wallet Address
+                  Store&rsquo;s name
+
                 </label>
-                <input className="block w-full px-6 py-3 text-black bg-white border border-gray-200 appearance-none rounded-xl placeholder:text-gray-400 focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm" placeholder="Your name"/>
+                <input className="block w-full px-6 py-3 text-black bg-white border border-gray-200 appearance-none rounded-xl placeholder:text-gray-400 focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm" placeholder="Store's name"/>
               </div>
-              <div className="col-span-full">
-                <label className="block mb-3 text-sm font-medium text-gray-600" id="company">
-                  What is the name of your company / organisation?
-                </label>
-                <input className="block w-full px-6 py-3 text-black bg-white border border-gray-200 appearance-none rounded-xl placeholder:text-gray-400 focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm" placeholder="Company name"/>
-              </div>
-              <div className="col-span-full">
-                <label className="block mb-3 text-sm font-medium text-gray-600" id="email">
-                  How shall we contact you?
-                </label>
-                <input className="block w-full px-6 py-3 text-black bg-white border border-gray-200 appearance-none rounded-xl placeholder:text-gray-400 focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm" placeholder="email@example.com" autoComplete="off" type="email"/>
-              </div>
-              <div>
-                <div>
-                  <label className="block mb-3 text-sm font-medium text-gray-600" id="message">
-                    Project details
-                  </label>
-                  <div className="mt-1">
-                    <textarea className="block w-full px-6 py-3 text-black bg-white border border-gray-200 appearance-none rounded-xl placeholder:text-gray-400 focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm" placeholder="What are you working on?" rows={4}></textarea>
-                  </div>
-                </div>
-              </div>
+            
               <div className="col-span-full">
                 <button className="items-center justify-center w-full px-6 py-2.5 text-center text-white duration-200 bg-black border-2 border-black rounded-full nline-flex hover:bg-transparent hover:border-black hover:text-black focus:outline-none focus-visible:outline-black text-sm focus-visible:ring-black" type="submit">
-                  Generate QR Code
+                  Generate Store
                 </button>
               </div>
             </div>
